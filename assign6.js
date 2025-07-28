85bd99227648f1bd577a01357201b7a6455013c2
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:55:11 +0530

    Add files via upload

---
+++ assign6.js
@@ -0,0 +1,157 @@
+//1.
+//ans:
+// let a=3;
+// if(a%2!==0){
+//     console.log("odd number");
+// }
+// else{
+//     console.log("even number");
+// }
+
+
+
+//2.
+//ans:
+// let stu_marks=100;
+// if(stu_marks>=90){
+//     console.log("A");
+// }
+// else if(stu_marks>=80){
+//     console.log("B");
+// }
+// else if(stu_marks>=70){
+//     console.log("c")
+// }
+// else if(stu_marks>=60){
+//  console.log("D")
+// }
+// else{
+//     console.log("F")
+// }
+
+
+
+//3.
+//ans:
+// let year=2000;
+// if(year%4==0 && (year%100!=0 ||year%400==0)){
+//     console.log("Leap year");
+// }
+// else{
+//     console.log("Not a leap year")
+// }
+
+
+
+//4.
+//ans:
+// let a=5;
+// let b=19;
+// let c=30;
+// if(a>b && a>c){
+//     console.log("a is bigger");
+// }
+// else if(b>a && b>c){
+//     console.log("b is bigger");
+// }
+// else if(c>a && c>b){
+//     console.log("c is bigger");
+// }
+// else{
+//     console.log("pls give valid numbers")
+// }
+
+
+//5.
+//ans:
+// let a=5;
+// let b=10;
+// let operation="multiplication"
+// switch(operation){
+//     case "addition":console.log(a+b);
+//     break;
+//     case "subtraction":console.log(a-b);
+//     break;
+//     case 'multiplication':console.log(a*b);
+//     break;
+//     case 'division':console.log(a/b);
+//     break;
+//     default:console.log("invalid operaion")
+// }
+
+
+//6.
+//ans:
+// let table=12;
+// for(let i=1;i<=10;i++){
+//     console.log(`${table} x ${i}= ${table * i}`)
+// }
+
+
+
+//7.
+//ans: 
+// let name="chowdaiah";
+// let vowelcount=0
+// let constcount=0
+// for(let i=0;i<name.length;i++){
+//     if(name[i]=="a" || name[i]=="e" || name[i]=="i" || name[i]=="o" || name[i]=="u"){
+//         vowelcount=vowelcount+1
+//     }
+//     else{
+//       constcount=constcount+1
+//     }
+// }
+// console.log("vowel count:",vowelcount);
+// console.log("const count:",constcount)
+
+
+
+//8.
+//ans:
+// for(let i=1;i<=50;i++){
+//     if(i%3==0){
+//         console.log("Fizz");
+//     }
+//     else if(i%5==0){
+//         console.log("Buzz");
+//     }
+//     else if(i%3==0 && i%5==0){
+//         console.log("FizzBuzz")
+//     }
+//     else{
+//         console.log(i)
+//     }
+    
+// }
+
+
+//9.
+//ans:
+// let month="may";
+// switch(month){
+//     case "january":console.log("jan has 31 days");
+//     break;
+//     case "february":console.log("feb has 28 days");
+//     break;
+//     case "march":console.log("march has 31 days");
+//     break;
+//     case "april":console.log("april has 30 days");
+//     break;
+//     case "may":console.log("may has 31 days");
+//     break;
+//     default:console.log("invalid montn");
+//     break;
+// }
+
+
+//10.
+//ans:
+// let sum=0;
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//         sum=sum+i
+//     }
+// }
+// console.log(sum)
+
