dbb62b61fac2901c84cd2623a8a65fb4906d1025
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:44:15 +0530

    Add files via upload

---
+++ assign3.js
@@ -0,0 +1,64 @@
+// 1.
+// ans:
+// let a=20;
+// let b=10;
+// let c=a+b;
+// console.log(c)
+// 2.
+// ans:
+// let x=10;
+// x-=3;
+// console.log(x)
+// 3.
+// ans:
+// let a=10;
+// let b=20;
+// let c=a*b;
+// console.log(c)
+// 4.
+// ans:
+// let n=20;
+// n-=5
+// console.log(n)
+//5.
+//ans:
+// The difference between pre-increment and post-increment is that pre-increment increments the value before it is used in an expression.
+// let a=10
+// let b=++a;
+// console.log(a,b)
+//6.
+//ans:
+// let x = 5;
+// let y = x++;
+// console.log(x, y);
+//output:6,5
+// in this case , x is incremented after the value os x assigned to y 
+//7.
+//ans:
+// let x = 5;
+// let y = ++x;
+// console.log(x, y);
+// output:6,6
+// in this case x is incremented before the value of x assigned to y
+//8.
+//ans:
+// let a=17;
+// let b=4;
+// console.log(a%b);
+//output:1
+//9.
+//ans:
+// let m=10;
+// m/=2;
+// console.log(m);
+//output:5
+//10.
+//ans:
+// let a=10;
+// let b=--a;
+// console.log(a,b)
+// //output:9.9
+// let a=10;
+// let b=a--;
+// console.log(a,b)
+// output:9,10
