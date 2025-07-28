c56a1297866d54ecf05c1522cc961971f4ec8a86
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:45:31 +0530

    Add files via upload

---
+++ assign4.js
@@ -0,0 +1,49 @@
+
+// 1.
+// ans:
+
+// let a=10;
+// console.log(typeof a);
+// let b="chowdaiah";
+// console.log(typeof b);
+// let c=true;
+// console.log(typeof c);
+// let d;
+// console.log(typeof d);
+// let e=null;
+// console.log(typeof e);
+// 3.
+// ans:
+// let container=10;
+// container +=5;
+// container -=2;
+// container *=3;
+// container /=2;
+// console.log(container);
+// 4.
+// ans:
+// let a=10;
+// let b=20;
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a===b);
+// console.log(a<=b);
+// 5.
+// ans:
+// let a=true;
+// let b=false;
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!a);
+// console.log(!b);
+//8.
+//ans:
+// console.log(5=="5");//the == checks for only value not data type
+// console.log(5==="5")//the === checks for both values and data types
+//10
+//ans.
+// let result = (10 > 5) && (5 < 3) || !(2 == 2);
+// console.log(result);// the and operator has higher precedence than the or operator,so it the expression will takes and operator value;
