00320cd76297e417b7e1b2099a4f22ff2e50e8d3
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:33:39 +0530

    Add files via upload

---
+++ mock1.js
@@ -0,0 +1,77 @@
+//1.
+//ans:
+let age=21;
+console.log(typeof age);
+//2.
+//ans:
+let price="150";
+let numprice=Number(price);
+console.log(typeof numprice);
+
+//3.
+//ans:
+let num1=10;
+let num2=5;
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+
+
+
+//5.
+//ans:
+let a=10;
+let b="5";
+let c=2;
+b=Number(b);
+console.log(a+b+c);
+
+//6.
+//ans:
+let score;
+console.log(score);
+console.log(typeof score);
+score=50;
+console.log(score);
+console.log(typeof score);
+
+//7.
+//ans:
+ var a=10;
+ var a=20;
+ console.log(a);//20 variable var can be redeclared and reassigned;
+ let b=20;
+ let b=30;
+ console.log(b);//syntax error:because using let we cannot redaclared we can reassigned;
+ const c=30;
+ const c=40;
+ console.log(c);//syntax error:because using const we cant redeclare and reassigne the values;
+
+//8.
+//ans:
+ let base=10
+ let height="5"
+ height=Number(height);
+ let area=(0.5*base*height);
+ console.log(area);
+
+//9.
+//ans:
+  let marks="85"
+ marks=Number(marks);
+ console.log(marks+15);
+
+//10.
+//ans:
+ let message="Hello, World!";
+ message=42;
+ console.log(message);
+ console.log(typeof message);
+
+//11.
+//ans:
+let text="123abc";
+text=Number(text);
+console.log(text)
+console.log(typeof text);
