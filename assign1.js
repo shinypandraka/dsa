7c2d2bda27b7c6402c14f7a62a1a750a2aef4ee9
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:40:08 +0530

    Add files via upload

---
+++ assign1.js
@@ -0,0 +1,35 @@
+<!-- 1.
+ans:
+A variable is a container to store values
+2.
+ans:
+we can declare by using var keyword
+var a=10;
+console.log(a)
+3.
+ans:The differnce between let and const is 
+ let once we declared and assigned we cant redeclare in let but we can reasigned
+ for const once we declared assigned we cant redeclare and we cant reasigned
+4.
+ans:we cant reassigned a value to const
+const a=10;
+const a=20;
+console.log(a)
+5.
+in js it  keyword is mandatory to use before variable name.with let local scope
+6.
+ans:variable hoisting means scope of the variable
+7.
+ans:
+let a=10;
+console.log(a)
+8. -->
+<!-- ans:
+<!-- No it is not possible to declare multiple variables in single line -->
+<!-- 9.
+ans:undefined
+10.
+ans:
+global means we can use any where but local means we cant use anywhere we can only use in a restricted way
+let is a local 
+var is a global --> -->
