60cf13101743530d12725b4e97352941081a5808
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:42:57 +0530

    Add files via upload

---
+++ assign2.js
@@ -0,0 +1,61 @@
+// 1.
+// ans:The primitive data types meand it hold only one value here some primitive data types
+//  number,string,boolean,null,undefined,bigint,symbol
+//  let a=20;, let b="chowdaiah";, let c=true;, let d=;, let e=null; let f=123345455n;
+// 2.
+// ans:The difference between let var cost
+// let we can declared only once through let but we can assign multiple times
+// var we can declared more times through var we can assign multiple times  
+// const we can only assgin and declare only once
+// 3.
+// ans:
+// null:Represents the intentional absence of an object value. 
+// undefined:Represents the intentional absence of an object value.
+// 4.
+// ans:
+// true;
+// false; 
+// the == does converts string into number
+// the === checks the type of data
+// 5.
+// let a=10;
+//  a=20
+//  console.log(a)
+// 6.
+// // ans:
+// let a=10;
+// console.log(typeof a)
+// 7.
+// ans:In javascript evetything is object
+// let a= null;
+// console.log(typeof a)
+//8.
+// ans:array is a collection of different data types at a Time
+// object is collection of key value Pairs
+//9.
+// ans:undefined
+//     undefined
+// 10.
+// let a="chowdaiah";
+// let b=10;
+// let c=true;
+// let a=19;
+// console.log(a)
+// console.log(typeof a)
+// let a="chowdaiah"
+// console.log(a)
+// console.log(typeof a)
+// let a=true;
+// console.log(a)
+// console.log(typeof a)
+// let a;
+// console.log(a)
+// console.log(typeof a)
+// let a=null;
+// console.log(a)
+// console.log(typeof a)
+// let a=new String("chowdaiah")
+// console.log(typeof a)
+// let a=new Number(10);
+// console.log(typeof a)
+// console.log(a)
