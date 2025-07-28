791ac6be122de92866dacef0ee4ed0d4cfbc7b39
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:53:51 +0530

    Add files via upload

---
+++ assign5.js
@@ -0,0 +1,194 @@
+//1.
+//ans:
+// let UserName="chowdaiah";
+// console.log(typeof UserName)
+
+//2.
+//ans:
+// let a;
+// let b=null;
+// console.log(typeof a)//undefined means variable is declared but not assigned any value
+// console.log(typeof b)//null means variable is declared and assigned null value
+
+
+//3.
+//ans:
+// let x=42;
+// if (x>0){
+//     console.log("x is a number")
+// }
+
+
+//4
+//ans:
+// let age="18"
+// let minage=18;
+// console.log(age==minage);
+// console.log(age===minage);
+
+//5.
+//ans:
+// let a=10;
+// let b=20;
+// if (a>b){
+//     console.log("greater number")
+// }
+// else{
+//     console.log("smaller number")
+// }
+
+//6.
+//ans:
+// let score=45;
+// if (score>=90){
+//     console.log("Excellent");
+// }
+// else if(score>=75){
+//     console.log("Good")
+// }
+// else if(score>=50){
+//     console.log("Average")
+// }
+// else{
+//     console.log("Below Average")
+// }
+
+
+//7.
+//ans:
+// let value="";
+// if (value==""){
+//     console.log("Value is empty")
+// }
+
+
+//9.
+//ans:
+// let n=17;
+// if (n%2==0){
+//     console.log("Even number")
+// }
+// else{
+//     console.log("Odd number")
+// }
+
+
+//10.
+//ans:
+// let temp;
+// if(temp ==undefined){
+//     console.log("Undefined")
+// }
+// else{
+//     console.log("Defined")
+// }
+
+
+//11.
+//ans:
+// let x=null;
+// if(x==null){
+//     console.log("Null value")
+// }
+// else{
+//     console.log("Defined")
+// }
+
+//12.
+//ans:
+// let num=-42;
+// if (num>10){
+//     console.log("positive")
+// }
+// else if(num>=-42){
+//     console.log("negative")
+// }
+// else{
+//     console.log("zero")
+// }
+
+//13.
+//ans:
+// let month="May";
+// if(month=="March"|| month=="April" || month=="May"){
+//     console.log("Spring");
+// }
+// else{
+//     console.log("Not Spring");
+// }
+
+//14.
+//ans:
+// let temp=25;
+// if(temp>20 && temp<30){
+//     console.log("Comfortable");
+// }
+// else {
+//     console.log("Not Comfortable")
+// }
+
+//15.
+//ans:
+// let year=2020;
+// if(year%4==0 && (year%100!=0 || year%400==0)){
+//     console.log("leap year");
+// }
+// else{
+//     console.log("Not a leap year");
+// }
+
+//16
+//ans:
+// let val;
+// console.log(typeof val);//we are declaring a variable but not assigned any value so it is undefined
+
+// val = 5;//here we are assigning a value to the variable
+// console.log(typeof val);//number
+// val = "hello";//again we are reassigning the value 
+// console.log(typeof val);//string
+//18
+//ans:
+// let person={name:"Asha"};
+// console.log(typeof person)
+
+//19.
+//ans:
+// let price=NaN;
+// if(price===NaN){
+//     console.log("Not a number");
+// }
+// else{
+//     console.log("Valid number")
+// }
+
+//20.
+//ans:
+// let x=15;
+// if(x%3==0 && x%5==0){
+//     console.log("divisible 3 and 5")
+// }
+// else if(x%3){
+//     console.log("divisible by 3")
+// }
+// else if(x%5){
+//     console.log("divisible by 5")
+// }
+// else{
+//     console.log("not divisible by 3 or 5")
+// }
+
+//21.
+//ans:
+// let marks=85;
+// if(marks>=75){
+//     console.log("Distinction")
+//     if(marks>=90){
+//         console.log("Outstanding")
+//     }
+//     else{
+//         console.log("very good")
+//     }
+// }
+// else{
+//     console.log("Not Distinction")
+// }
