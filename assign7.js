7e37b16945e9bc8972d992860379f657172cf303
shinypandraka <Shinypandraka@gmail.com>
2025-07-28 11:56:11 +0530

    Add files via upload

---
+++ assign7.js
@@ -0,0 +1,93 @@
+//1.
+//ans:
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+//2.
+//ans:
+// let count=[];
+// for(let i=1;i<=20;i++){
+//     if(i%2==0){
+//         count=count+i+"\n"
+//     }
+// }
+// console.log(count)
+
+
+//3.
+//ans:
+// for(let i=10;i>=1;i--){
+//     console.log(i)
+// }
+
+
+//4.
+//ans:
+// let sum=0;
+// for(let i=1;i<=10;i++){
+//     sum=sum+i
+// }
+// console.log(sum);
+
+
+//5
+//ans:
+// for(let i=1;i<=5;i++){
+//     let count="*";
+//     for(let j=1;j<i;j++){
+//         count=count+"*"
+//     }
+//     console.log(count)
+// }
+
+//6.
+//ans:
+// for(let i=1;i<=5;i++){
+//     let count="*"
+//     for(let j=1;j<=5;j++){
+//         count=count+"*"
+//     }
+//     console.log(count)
+
+// }
+
+
+//7.
+//ans:
+// let table=7;
+// for(let i=1;i<=10;i++){
+//     console.log(`${table}X${i}=${table*i}`)
+// }
+
+
+
+//8.
+//ans:
+// for(let i=1;i<=5;i++){
+//     let count="";
+//     for(let j=1;j<=i;j++){
+//         count=count+j
+//     }
+//     console.log(count)
+   
+// }
+
+//9.
+//ans:
+// for(let i=1;i<=50;i++){
+//     if(i%2!=0){
+//         console.log(i)
+//     }
+// }
+
+
+//10
+//ans:
+// for(let i=1;i<=5;i++){
+//     let count="";
+//     for(let j=1;j<=i;j++){
+//         count=count+i
+//     }
+//     console.log(count)
+// }
